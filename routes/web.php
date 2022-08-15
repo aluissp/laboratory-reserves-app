aad9e606578263fc45a729853340c4518cd491db
aluissp <luisprg05@gmail.com>
2022-08-14 22:15:40 -0500

    Agregado config\role.php

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => auth()->check() ? route('home') : view('welcome'));
 
 Auth::routes();
 
